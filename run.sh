ec474d5938225b33a4268fe06eb798b24c798a18
System Informacji Wczesnego Ostrzegania <siwo@siwo24.pl>
2015-08-05 20:57:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ if [ ! -f /.tomcat_admin_created ]; then
     /create_tomcat_admin_user.sh
 fi
 
+rm -rf ${CATALINA_HOME}/webapps/*
+
+ sshpass -p ${SIWO_SSH_PASSWORD} scp -o StrictHostKeyChecking=no ${SIWO_SSH_USER}@${SIWO_IPADDRESS}:${SIWO_PATH_TO_APP}/${SIWO_APP_DIR}/${SIWO_APP_NAME}##${SIWO_APP_VERSION}.war ${CATALINA_HOME}/webapps
+
 exec ${CATALINA_HOME}/bin/catalina.sh run
