a5e20b47dbf2ccf19955452550d2bfd0776cb360
System Informacji Wczesnego Ostrzegania <siwo@siwo24.pl>
2015-08-05 20:54:46 +0200

    remove

--- run.sh
+++ run.sh
@@ -5,7 +5,5 @@ if [ ! -f /.tomcat_admin_created ]; then
 fi
 
 rm -rf ${CATALINA_HOME}/webapps/*
-sshpass -p 'Bartolomeo840201' scp -o StrictHostKeyChecking=no root@backup.siwo.org.pl:/opt/deploy/siwo/${SIWO_APP}/ROOT##${SIWO_VERSION}.war ${CATALINA_HOME}/webapps
-
 
 exec ${CATALINA_HOME}/bin/catalina.sh run
